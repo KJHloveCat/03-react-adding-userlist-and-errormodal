4e546a9e1af3db131e877592f206f0e416e94cb1
KJHloveCat <kjh99407@gmail.com>
2023-01-19 16:54:29 +0900

    0119

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import AddUser from "./Components/User/AddUser";
+import UserList from "./Components/User/UsersList";
 
 function App() {
+  const [userList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge, id) => {
+    setUsersList((prevList) => {
+      return [
+        ...prevList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddUser onAddUser={addUserHandler}></AddUser>
+      <UserList users={userList}></UserList>
     </div>
   );
 }
---
+++ src/Components/UI/ErrorModal.js
@@ -0,0 +1,27 @@
+import styles from "./ErrorModal.module.css";
+import Card from "./Card";
+import Button from "./Button";
+
+const ErrorModal = (props) => {
+  const errorHandler = () => {
+    props.error("");
+  };
+  return (
+    <div>
+      <div className={styles.backdrop} />
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={errorHandler}>Okay</Button>
+        </footer>
+      </Card>
+    </div>
+  );
+};
+
+export default ErrorModal;
